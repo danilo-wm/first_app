6b8cefbe4b83c1739fbd5c804d78f3ce0a52d2f5
Weedmaps <weedmaps@Weedmapss-MacBook-Air.local>
2013-06-11 17:49:17 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '61ec23de1fa0d0673d92af3819b3222f06e40e6260a19eab2d9a9ea81b758f38314a0531ca4df04c2ae932f0ec0ea1daa07e94eb10fd9fdebaaa997300fa6b1b'
